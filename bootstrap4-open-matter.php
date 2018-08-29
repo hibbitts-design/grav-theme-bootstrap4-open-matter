40d12d925463bcba0ad9c319be6658b389cb1da9
Paul Hibbitts <paul_hibbitts@sfu.ca>
2018-08-29 13:29:13 -0700

    Added insert PDF Shortcode button into Editor toolbar.

--- bootstrap4-open-matter.php
+++ bootstrap4-open-matter.php
@@ -31,8 +31,9 @@ class Bootstrap4OpenMatter extends Theme
     public function onTwigSiteVariables()
     {
         if ($this->isAdmin() && ($this->grav['config']->get('plugins.shortcode-core.enabled'))) {
+            $this->grav['assets']->add('theme://editor-buttons/admin/js/shortcode-pdf.js');
             $this->grav['assets']->add('theme://editor-buttons/admin/js/shortcode-h5p.js');
-      }
+        }
     }
 
 }
