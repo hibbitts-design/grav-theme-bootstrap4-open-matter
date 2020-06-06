7080b1d9824b0a33ad1cc9a32f1eab5f95132ea6
Paul Hibbitts <paul@paulhibbitts.com>
2020-06-06 10:16:58 -0700

    Added ShowIfEmbedded and HideIfEmbedded Shortcodes to only show content when embedded.

---
+++ shortcodes/HideIfEmbeddedShortcode.php
@@ -0,0 +1,14 @@
+<?php
+namespace Grav\Plugin\Shortcodes;
+
+use Thunder\Shortcode\Shortcode\ShortcodeInterface;
+
+class HideIfEmbeddedShortcode extends Shortcode
+{
+    public function init()
+    {
+        $this->shortcode->getHandlers()->add('hideifembedded', static function(ShortcodeInterface $sc) {
+            return '<div class="chromeless-hide";>' . $sc->getContent() . '</div>';
+        });
+    }
+}
---
+++ shortcodes/ShowIfEmbeddedShortcode.php
@@ -0,0 +1,14 @@
+<?php
+namespace Grav\Plugin\Shortcodes;
+
+use Thunder\Shortcode\Shortcode\ShortcodeInterface;
+
+class ShowIfEmbeddedShortcode extends Shortcode
+{
+    public function init()
+    {
+        $this->shortcode->getHandlers()->add('showifembedded', static function(ShortcodeInterface $sc) {
+            return '<div class="chromeless-show";>' . $sc->getContent() . '</div>';
+        });
+    }
+}
